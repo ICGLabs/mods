b9f0e240317aa322a1bad29d79f2b2691e56ebd3
Elad Karako <eladkarako@users.noreply.github.com>
2018-10-31 12:36:52 +0200

    adding 'version' to the info, just to make it more clear what version it is...

--- Aria2/_update_readme.cmd
+++ Aria2/_update_readme.cmd
@@ -1,2 +1,10 @@
 @echo off
-call aria2c.exe --help=#all   2>&1  >readme_aria2c.nfo
+chcp 65001 2>nul >nul
+echo.>readme_aria2c.nfo
+call aria2c.exe --version     2>&1  >>readme_aria2c.nfo
+echo.>>readme_aria2c.nfo
+echo.>>readme_aria2c.nfo
+echo.-------------------------------------------------------------------------------------->>readme_aria2c.nfo
+echo.>>readme_aria2c.nfo
+echo.>>readme_aria2c.nfo
+call aria2c.exe --help=#all   2>&1  >>readme_aria2c.nfo
